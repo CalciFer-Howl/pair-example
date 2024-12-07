a530ed2f7ffd7f4fc7933dd45e579409a8d35f5e
⚡ CalciFer <168836504+CalciFer-Howl@users.noreply.github.com>
2024-12-07 20:18:43 +0530

    Fixed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
+  PREFIX: process.env.PREFIX || "𝐂𝐫𝐨𝐧𝐞𝐱𝐁𝐨𝐭~",
+  EMAIL: process.env.EMAIL || "ameenxnt@gmail.com",
   PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
